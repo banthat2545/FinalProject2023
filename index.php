7a896de6ac6eb48f2658aa4aaa6838e981acde77
banthat2545 <152368283+banthat2545@users.noreply.github.com>
2023-12-20 17:20:20 +0700

    first upload

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <!-- Basic Page Needs
+================================================== -->
+  <meta charset="utf-8">
+  <title>Constra - Construction Html5 Template</title>
+
+  <!-- Mobile Specific Metas
+================================================== -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="Construction Html5 Template">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+  <meta name=author content="Themefisher">
+  <meta name=generator content="Themefisher Constra HTML Template v1.0">
+
+  <!-- Favicon
+================================================== -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+  <!-- CSS
+================================================== -->
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
+  <!-- FontAwesome -->
+  <link rel="stylesheet" href="plugins/fontawesome/css/all.min.css">
+  <!-- Animation -->
+  <link rel="stylesheet" href="plugins/animate-css/animate.css">
+  <!-- slick Carousel -->
+  <link rel="stylesheet" href="plugins/slick/slick.css">
+  <link rel="stylesheet" href="plugins/slick/slick-theme.css">
+  <!-- Colorbox -->
+  <link rel="stylesheet" href="plugins/colorbox/colorbox.css">
+  <!-- Template styles-->
+  <link rel="stylesheet" href="css/style.css">
+
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Prompt&display=swap');
+
+    .slide-title,
+    .slide-sub-title,
+    .slider {
+      font-family: 'Prompt', sans-serif;
+    }
+  </style>
+
+</head>
+
+<body>
+  <div class="body-inner">
+
+    <div class="banner-carousel banner-carousel-1 mb-0">
+      <div class="banner-carousel-item" style="background-image:url(images/slider-main/bg_4.jpg)">
+        <div class="slider-content">
+          <div class="container h-100">
+            <div class="row align-items-center h-100">
+              <div class="col-md-12 text-center">
+                <h2 class="slide-title" data-animation-in="slideInLeft">มหาวิทยาลัยเทคโนโลยีราชมงคลอีสาน วิทยาเขตขอนแก่น</h2>
+                <h3 class="slide-sub-title" data-animation-in="slideInRight">ชมรมอนุรักษ์ธรรมชาติและสิ่งแวดล้อม</h3>
+                <p data-animation-in="slideInLeft" data-duration-in="1.2">
+                  <a href="../club_app/member/login.php" class="slider btn btn-primary">สมาชิก</a>
+                  <a href="../club_app/admin_1/login.php" class="slider btn btn-primary border">คณะกรรมการชมรม</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="banner-carousel-item" style="background-image:url(images/slider-main/bg_6.jpg)">
+        <div class="slider-content text-left">
+          <div class="container h-100">
+            <div class="row align-items-center h-100">
+              <div class="col-md-12">
+                <h2 class="slide-title-box" data-animation-in="slideInDown">คณะกรรมการชมรม</h2>
+                <h3 class="slide-title" data-animation-in="fadeIn">ให้นักศึกษาได้รู้จัก สัมผัส ธรรมชาติที่แท้จริง เกิดความรู้สึกรัก และหวงแหน <br>ช่วยกันดูแลรักษาธรรมชาติและสิ่งแวดล้อมไว้ให้อยู่กับพวกเราไปนานๆ</h3>
+                <h3 class="slide-sub-title" data-animation-in="slideInLeft">ชมรมอนุรักษ์ธรรมชาติและสิ่งแวดล้อม</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="banner-carousel-item" style="background-image:url(images/slider-main/bg_5.jpg)">
+        <div class="slider-content text-right">
+          <div class="container h-100">
+            <div class="row align-items-center h-100">
+              <div class="col-md-12">
+                <h3 class="slide-sub-title" data-animation-in="fadeIn">Preserve nature and the environment</h3>
+                <p class="slider-description lead" data-animation-in="slideInRight">Rajamangala University of Technology Isan Khon Kaen Campus.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section class="call-to-action-box no-padding">
+      <div class="container">
+        <div class="action-style-box bg-warning">
+          <div class="row align-items-center">
+            <div class="col-md-8 text-center text-md-left">
+              <div class="call-to-action-text ">
+                <h3 class="action-title">ชมรมอนุรักษ์ธรรมชาติและสิ่งแวดล้อม</h3>
+              </div>
+            </div><!-- Col end -->
+          </div><!-- row end -->
+        </div><!-- Action style box -->
+      </div><!-- Container end -->
+    </section><!-- Action end -->
+
+    <script src="plugins/jQuery/jquery.min.js"></script>
+    <!-- Bootstrap jQuery -->
+    <script src="plugins/bootstrap/bootstrap.min.js" defer></script>
+    <!-- Slick Carousel -->
+    <script src="plugins/slick/slick.min.js"></script>
+    <script src="plugins/slick/slick-animation.min.js"></script>
+    <!-- Color box -->
+    <script src="plugins/colorbox/jquery.colorbox.js"></script>
+    <!-- shuffle -->
+    <script src="plugins/shuffle/shuffle.min.js" defer></script>
+
+
+    <!-- Google Map API Key-->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcABaamniA6OL5YvYSpB3pFMNrXwXnLwU" defer></script>
+    <!-- Google Map Plugin-->
+    <script src="plugins/google-map/map.js" defer></script>
+
+    <!-- Template custom -->
+    <script src="js/script.js"></script>
+
+  </div><!-- Body inner end -->
+</body>
+
+</html>
